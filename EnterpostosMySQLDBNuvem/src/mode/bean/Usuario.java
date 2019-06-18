0151d543c5d8aa624092848d41f12de65a84266a
Guilherme Serpa Batista <serpa419@msn.com>
2019-06-18 09:52:04 -0300

    Adicionado Sistema DB Externo
    *Sistema compilado e configurado para servidor em nuvem já funcionando.
    
    *****************************************
    
    Login: j.tomaz
    senha:admin
    
    *****************************************

---
+++ EnterpostosMySQLDBNuvem/src/mode/bean/Usuario.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mode.bean;
+
+/**
+ *
+ * @author serpa
+ */
+public class Usuario {
+    
+    private int Id;
+    private String usuario;
+    private String senha;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        this.Id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
